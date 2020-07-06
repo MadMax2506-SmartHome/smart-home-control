512e4ca0ebeeb1bedd805e3d1f3de92bf469e17d
Max Janorschke <mm.madmax2506@gmail.com>
2020-07-06 14:36:43 +0200

    SmartHome Control

---
+++ pages/smarthome_control/roomlight/lights/bed_side_screen.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { StyleSheet, ScrollView, View, Button } from 'react-native';
+
+import Light from "./light_control.js"
+
+import STYLE from '../../../../data/config/style.js'
+
+export default class BedScreen extends Component {
+	constructor(props) {
+    super(props);
+
+		const { params }  = this.props.route;
+
+		this.value 				= params.value;
+    this.contentData  = params.contentData;
+
+		this.data	= params.data;
+		this.mqtt = params.mqtt;
+
+		this.mqtt.topic.lightStatus = this.mqtt.topic.globalStatus + this.contentData.lights.topics[this.value];
+		this.mqtt.topic.lightConf = this.mqtt.topic.globalConf + this.contentData.lights.topics[this.value];
+  }
+
+  render() {
+		return(
+      <View>
+        <Light data={this.data} mqtt={this.mqtt} contentData={this.contentData} />
+      </View>
+    );
+  }
+}
---
+++ pages/smarthome_control/roomlight/lights/control_elements/orientation.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import ItemPicker from '../../../../../madmax_modules/item-picker/ItemPicker.js'
+
+export default class AnimationType extends Component  {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labels: this.props.labels,
+      values: this.props.values,
+      selectedValue: this.props.selectedValue,
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={style.header}>
+          <Text>
+            Richtung:
+          </Text>
+        </View>
+        <View style={style.main}>
+          <ItemPicker labels={this.state.labels} values={this.state.values} selectedValue={this.state.selectedValue} onChange={(orientation) => this.props.onChange(orientation)}/>
+        </View>
+      </View>
+    );
+  }
+};
+
+const style = StyleSheet.create({
+  header: {
+    width: "100%",
+  },
+  main: {
+    width: "100%",
+    paddingLeft: "3%",
+    paddingRight: "3%",
+  },
+});
