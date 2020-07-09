64eeedb01315ffb1a37ebfbd145896e2d702688f
Max Janorschke <mm.madmax2506@gmail.com>
2020-07-09 20:30:39 +0200

    Anpassung eigener Module

--- madmax_modules/loadData/LoadData.js
+++ madmax_modules/loadData/LoadData.js
@@ -3,23 +3,36 @@ import { StyleSheet, ActivityIndicator, View, Text, Button } from 'react-native'
 
 export default class ControlView extends Component {
   render() {
-    return(
-      <View style={style.main}>
-        <View>
-          <ActivityIndicator size="large" color="#00000" />
+    if(this.props.abort == null) {
+      return(
+        <View style={style.main}>
+          <View>
+            <ActivityIndicator size="large" color="#00000" />
+          </View>
+          <View style={style.info}>
+            <Text>{(this.props.text == undefined) ? '' : this.props.text}</Text>
+          </View>
         </View>
-        <View style={style.info}>
-          <Text>{(this.props.text == undefined) ? '' : this.props.text}</Text>
+      );
+    } else {
+      return(
+        <View style={style.main}>
+          <View>
+            <ActivityIndicator size="large" color="#00000" />
+          </View>
+          <View style={style.info}>
+            <Text>{(this.props.text == undefined) ? '' : this.props.text}</Text>
+          </View>
+          <View style={style.btn}>
+            <Button
+              title="Abbrechen"
+              color = "#000000"
+              onPress={() => this.props.abort()}
+            />
+          </View>
         </View>
-        <View style={style.btn}>
-          <Button
-            title="Abbrechen"
-            color = "#000000"
-            onPress={() => this.props.navigation.goBack()}
-          />
-        </View>
-      </View>
-    );
+      );
+    }
   }
 }
 
--- madmax_modules/sqlite/DB.js
+++ madmax_modules/sqlite/DB.js
@@ -1,89 +1,209 @@
 import { openDatabase } from 'react-native-sqlite-storage';
 
-const DB_NAME         = "smarthome.db";
-const DB_TABLE_GLOBAL = "smarthome";
-const DB_TABLE_MQTT   = "mqtt";
-const DB_TABLE_NAS    = "nas";
-const DB_TABLE_USER   = "user";
+const DB_NAME           = "smarthome.db";
+const DB_TABLE_FEATURE  = "feature";
+const DB_TABLE_USER     = "user";
+const DB_TABLE_MQTT     = "mqtt";
+const DB_TABLE_NAS      = "nas";
 
 export default class DB {
   constructor(class_) {
     this.class_ = class_;
     this.con    = openDatabase({ name: DB_NAME });
 
+    this.load_data = {
+      feature: false,
+      user: false,
+      mqtt: false,
+      nas: false,
+    }
     this.data = {
-      user: {},
-      mqtt: {},
-      nas: {}
+      feature: {
+        is_smart_home_control_active: null,
+        is_nas_control_active: null,
+      },
+      user: {
+        first_name: null,
+        surname: null,
+      },
+      mqtt: {
+        typ: null,
+        ipaddress: null,
+        port: null,
+      },
+      nas: {
+        ipaddress: null,
+        macaddress: null,
+        username: null,
+        password: null,
+      }
     }
 
     this.create();
 
+    this.select_feature();
     this.select_user();
     this.select_mqtt();
     this.select_nas();
   }
 
+  is_data_load() {
+    return (this.load_data.feature && this.load_data.user && this.load_data.mqtt && this.load_data.nas) == true ? true : false
+  }
+
+  get_data() {
+    return this.data
+  }
+
   set_data(category, data) {
     if(data != null) {
-      this.data[category] = data
+      if(category == "feature") {
+        data.is_smart_home_control_active = Boolean(data.is_smart_home_control_active)
+        data.is_nas_control_active        = Boolean(data.is_nas_control_active)
+      }
+
+      this.data[category]       = data
+      this.load_data[category]  = true
     }
   }
 
   create() {
+    // feature
+    var sql_feature = "CREATE TABLE IF NOT EXISTS " + DB_TABLE_FEATURE + "("
+      + "feature_id int,"
+      + "is_smart_home_control_active bool,"
+      + "is_nas_control_active bool,"
+      + "PRIMARY KEY(feature_id));";
+
     // user
     var sql_user = "CREATE TABLE IF NOT EXISTS " + DB_TABLE_USER + "("
-      + "userid int,"
+      + "user_id int,"
       + "first_name char(255),"
       + "surname char(255),"
-      + "PRIMARY KEY(userid));";
+      + "PRIMARY KEY(user_id));";
 
     // mqtt
     var sql_mqtt = "CREATE TABLE IF NOT EXISTS " + DB_TABLE_MQTT + "("
-      + "mqttid int,"
+      + "mqtt_id int,"
       + "typ char(10),"
       + "ipaddress char(15),"
       + "port int,"
-      + "PRIMARY KEY(mqttid));";
+      + "PRIMARY KEY(mqtt_id));";
 
     // nas
     var sql_nas = "CREATE TABLE IF NOT EXISTS " + DB_TABLE_NAS + "("
-      + "nasid int,"
+      + "nas_id int,"
       + "ipaddress char(15),"
       + "macaddress char(12),"
       + "username char(255),"
       + "password char(255),"
-      + "PRIMARY KEY(nasid));";
+      + "PRIMARY KEY(nas_id));";
 
     //sql
     this.con.transaction(tx => {
+      tx.executeSql(sql_feature, []);
       tx.executeSql(sql_user, []);
       tx.executeSql(sql_mqtt, []);
       tx.executeSql(sql_nas, []);
     });
   }
 
+// feature
+  is_feature_data_empty() {
+    let { feature } = this.data
+
+    if(feature.is_smart_home_control_active == null
+      && feature.is_nas_control_active == null) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get_feature_data() {
+    return this.data.feature
+  }
+
+  get_is_smart_home_control_active() {
+    return (this.data == null || this.data.feature.is_smart_home_control_active == null) ? '' : Boolean(this.data.feature.is_smart_home_control_active);
+  }
+
+  get_is_nas_control_active() {
+    return (this.data == null || this.data.feature.is_nas_control_active == null) ? '' : Boolean(this.data.feature.is_nas_control_active);
+  }
+
+  insert_feature(values) {
+    var sql_feature     = "INSERT INTO " + DB_TABLE_FEATURE + "(feature_id, is_smart_home_control_active, is_nas_control_active) VALUES(?, ?, ?);";
+
+    var values_feature  = [1, values.is_smart_home_control_active, values.is_nas_control_active];
+    this.con.transaction(tx => {
+      tx.executeSql(sql_feature, values_feature);
+    });
+  }
+
+  update_feature(values) {
+    var sql_feature     = "UPDATE " + DB_TABLE_FEATURE + " SET is_smart_home_control_active = ?, is_nas_control_active = ? WHERE feature_id = ?;";
+    var values_feature  = [values.is_smart_home_control_active, values.is_nas_control_active, 1];
+
+    this.con.transaction(tx => {
+      tx.executeSql(sql_feature, values_feature);
+    });
+  }
+
+  select_feature() {
+    var sql_feature = "SELECT is_smart_home_control_active, is_nas_control_active FROM " + DB_TABLE_FEATURE + ";"
+    this.con.transaction(tx => {
+      tx.executeSql(sql_feature, [], (tx, res) => {
+        var length = res.rows.length;
+        if(length > 0) {
+          var data = res.rows.item(0);
+          this.class_.set_data_from_sqlite("feature", data);
+        } else {
+          this.class_.set_data_from_sqlite("feature", null);
+        }
+      });
+    });
+  }
+
 // user
+  is_user_data_empty() {
+    let { user } = this.data
+
+    if(user.first_name == null
+      && user.surname == null) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get_user_data() {
+    return this.data.user
+  }
+
   get_name() {
-    return this.get_first_name() + " " + this.get_surname()
+  return (this.get_first_name() == "" || this.get_surname() == "") ? "" : this.get_first_name() + " " + this.get_surname()
   }
 
   get_first_name() {
-    return (this.data == null || this.data.first_name == null) ? '' : this.data.first_name;
+    return (this.data == null || this.data.user.first_name == null) ? '' : this.data.user.first_name;
   }
 
   get_surname() {
-    return (this.data == null || this.data.surname == null) ? '' : this.data.surname;
+    return (this.data == null || this.data.user.surname == null) ? '' : this.data.user.surname;
   }
 
   insert_user(values) {
-    var sql_user     = "INSERT INTO " + DB_TABLE_USER + "(userid, first_name, surname) VALUES(?, ?, ?);";
+    var sql_user     = "INSERT INTO " + DB_TABLE_USER + "(user_id, first_name, surname) VALUES(?, ?, ?);";
 
     var values_user  = [1, values.first_name, values.surname];
+    this.con.transaction(tx => {
+      tx.executeSql(sql_user, values_user);
+    });
   }
 
   update_user(values) {
-    var sql_user     = "UPDATE " + DB_TABLE_USER + " SET first_name = ?, surname = ? WHERE userid = ?;";
+    var sql_user     = "UPDATE " + DB_TABLE_USER + " SET first_name = ?, surname = ? WHERE user_id = ?;";
     var values_user  = [values.first_name, values.surname, 1];
 
     this.con.transaction(tx => {
@@ -100,7 +220,6 @@ export default class DB {
           var data = res.rows.item(0);
           this.class_.set_data_from_sqlite("user", data);
         } else {
-          this.insert_user({first_name: "", surname: ""})
           this.class_.set_data_from_sqlite("user", null);
         }
       });
@@ -108,30 +227,50 @@ export default class DB {
   }
 
 // mqtt
+  is_mqtt_data_empty() {
+    let { mqtt } = this.data
+
+    if(mqtt.typ == null
+      && mqtt.ipaddress == null
+      && mqtt.port == null) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get_mqtt_data() {
+    return this.data.mqtt
+  }
+
   get_mqtt_uri() {
-    return this.get_mqtt_type() + "://" + this.get_mqtt_ipaddress() + ":" + this.get_mqtt_port();
+    return (this.get_mqtt_type() == "" || this.get_mqtt_ipaddress() == "" || this.get_mqtt_port() == "") ? "" : this.get_mqtt_type() + "://" + this.get_mqtt_ipaddress() + ":" + this.get_mqtt_port();
   }
 
   get_mqtt_type() {
-    return (this.data == null || this.data.mqtt_type == null) ? 'mqtt' : this.data.mqtt_type;
+    return (this.data == null || this.data.mqtt.type == null) ? 'mqtt' : this.data.mqtt.type;
   }
 
   get_mqtt_port() {
-    return (this.data == null || this.data.mqtt_port == null) ? '' : this.data.mqtt_port.toString();
+    return (this.data == null || this.data.mqtt.port == null) ? '' : this.data.mqtt.port.toString();
   }
 
   get_mqtt_ipaddress() {
-    return (this.data == null || this.data.mqtt_ipaddress == null) ? '' : this.data.mqtt_ipaddress;
+    return (this.data == null || this.data.mqtt.ipaddress == null) ? '' : this.data.mqtt.ipaddress;
   }
 
   insert_mqtt(values) {
-    var sql_mqtt     = "INSERT INTO " + DB_TABLE_MQTT + "(mqttid, typ, ipaddress, port) VALUES(?, ?, ?, ?);";
+    var sql_mqtt     = "INSERT INTO " + DB_TABLE_MQTT + "(mqtt_id, typ, ipaddress, port) VALUES(?, ?, ?, ?);";
 
-    var values_mqtt  = [1, values.mqtt_typ, values.mqtt_ipaddress, parseInt(values.mqtt_port)];
+    var values_mqtt  = [1, values.typ, values.ipaddress, parseInt(values.port)];
+
+    this.con.transaction(tx => {
+      tx.executeSql(sql_mqtt, values_mqtt);
+    });
   }
 
   update_mqtt(values) {
-    var sql_mqtt     = "UPDATE " + DB_TABLE_MQTT + " SET typ = ?, ipaddress = ?, port = ? WHERE mqttid = ?;";
+    var sql_mqtt     = "UPDATE " + DB_TABLE_MQTT + " SET typ = ?, ipaddress = ?, port = ? WHERE mqtt_id = ?;";
     var values_mqtt  = [values.typ, values.ipaddress, parseInt(values.port), 1];
 
     this.con.transaction(tx => {
@@ -146,9 +285,9 @@ export default class DB {
         var length = res.rows.length;
         if(length > 0) {
           var data = res.rows.item(0);
+          data.port = data.port.toString()
           this.class_.set_data_from_sqlite("mqtt", data);
         } else {
-          this.insert_user({typ: "mqtt", ipaddress: "", port: ""})
           this.class_.set_data_from_sqlite("mqtt", null);
         }
       });
@@ -156,6 +295,19 @@ export default class DB {
   }
 
 // nas
+  is_nas_data_empty() {
+    let { nas } = this.data
+
+    if(nas.ipaddress == null
+      && nas.macaddress == null
+      && nas.username == null
+      && nas.password == null) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   get_nas_data() {
     return this.data.nas
   }
@@ -177,13 +329,17 @@ export default class DB {
   }
 
   insert_nas(values) {
-    var sql_nas      = "INSERT INTO " + DB_TABLE_NAS + "(nasid, ipaddress, macaddress, username, password) VALUES(?, ?, ?, ?, ?);";
+    var sql_nas      = "INSERT INTO " + DB_TABLE_NAS + "(nas_id, ipaddress, macaddress, username, password) VALUES(?, ?, ?, ?, ?);";
 
-    var values_nas   = [1, values.nas_ipaddress, values.nas_macaddress, values.nas_user, values.nas_password];
+    var values_nas   = [1, values.ipaddress, values.macaddress, values.username, values.password];
+
+    this.con.transaction(tx => {
+      tx.executeSql(sql_nas, values_nas);
+    });
   }
 
   update_nas(values) {
-    var sql_nas     = "UPDATE " + DB_TABLE_NAS + " SET ipaddress = ?, macaddress = ?, username = ?, password = ? WHERE nasid = ?;";
+    var sql_nas     = "UPDATE " + DB_TABLE_NAS + " SET ipaddress = ?, macaddress = ?, username = ?, password = ? WHERE nas_id = ?;";
     var values_nas  = [values.ipaddress, values.macaddress, values.username, values.password, 1];
 
     this.con.transaction(tx => {
@@ -200,7 +356,6 @@ export default class DB {
           var data = res.rows.item(0);
           this.class_.set_data_from_sqlite("nas", data);
         } else {
-          this.insert_user({ipaddress: "", macaddress: "", username: "", password: ""})
           this.class_.set_data_from_sqlite("nas", null);
         }
       });
