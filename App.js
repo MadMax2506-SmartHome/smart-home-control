9f215223b820f5b62142f1e70e7054bb2a65cffd
Max Janorschke <mm.madmax2506@gmail.com>
2020-07-11 20:34:42 +0200

    Warnungen unterdrücken

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 
+// Navigation
 import Navigation from './pages/navigation/Navigation.js'
 
+// Warnungen ausblenden
+console.disableYellowBox = true;
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <Navigation />
